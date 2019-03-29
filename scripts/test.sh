6ad63c299f693c7b86341db2e988573f75ff7ead
artlands <lijie0416@gmail.com>
2019-03-29 15:14:54 -0500

    add scripts

---
+++ scripts/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+STENCIL = ../stencil
+
+BSIZE = 256
+CAPACITY = 4
+NUMDIVS = 1
+STENTYPE = 1
+SIZEX = 1024
+SIZEY = 0
+SIZEZ = 0
+FILENAME = trace.out
+
+echo "Executing: $STENCIL -b $BSIZE -c $CAPACITY -d $NUMDIVS -t $STENTYPE\
+-x $SIZEX -y $SIZEY -z $SIZEZ -F $FILENAME"
+
+$STENCIL -b $BSIZE -c $CAPACITY -d $NUMDIVS -t $STENTYPE -x $SIZEX -y $SIZEY\
+-z $SIZEZ -F $FILENAME
