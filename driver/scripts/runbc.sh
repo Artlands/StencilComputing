1bcfe395b15d280742220f58476d9bae26bd9fad
Artlands <lijie0416@gmail.com>
2019-05-30 07:04:37 -0500

    update

--- driver/scripts/runbc.sh
+++ driver/scripts/runbc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BC=../bandconflict_driver
+BC=../bankconflict_driver
 
-TRACEFILE=/home/jieli/StencilComputing/traces/HOST-3D-7points-O2X64Y64Z64
+TRACEFILE=/home/jieli/StencilComputing/traces/PIMS-3D-25points-O8X64Y64Z64
 
 echo "Executing : $BC -f $TRACEFILE"
 
